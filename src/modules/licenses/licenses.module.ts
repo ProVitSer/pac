a76816264af81248eea229eb57a9e0c8001b7ee2
ProVitSer <prokinvs@gmail.com>
2024-08-19 23:12:15 +0200

    fix: add License entity

--- src/modules/licenses/licenses.module.ts
+++ src/modules/licenses/licenses.module.ts
@@ -1,8 +1,11 @@
 import { Module } from '@nestjs/common';
 import { LicensesService } from './services/licenses.service';
 import { LicensesController } from './controllers/licenses.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { License } from './entities/license.entity';
 
 @Module({
+    imports: [TypeOrmModule.forFeature([License])],
     providers: [LicensesService],
     controllers: [LicensesController],
 })
