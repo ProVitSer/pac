2719f4f1e7ceeda8600ee45c689407710d85ea86
ProVitSer <prokinvs@gmail.com>
2024-09-02 22:10:42 +0200

    feat: add some asterisk variables from env

--- docker/asterisk/configure_templates.sh
+++ docker/asterisk/configure_templates.sh
@@ -3,3 +3,4 @@ envsubst < /tmp/odbc.ini.template > /etc/odbc.ini
 envsubst < /tmp/res_odbc.conf.template > /etc/asterisk/res_odbc.conf
 envsubst < /tmp/ari.conf.template > /etc/asterisk/ari.conf
 envsubst < /tmp/manager.conf.template > /etc/asterisk/manager.conf
+envsubst < /tmp/http.conf.template > /etc/asterisk/http.conf
