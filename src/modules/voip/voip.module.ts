0b8d637a1eae3c44de64663d2bf23140d7435032
ProVitSer <prokinvs@gmail.com>
2024-09-06 19:51:21 +0000

    feat(voip): del no used controller

--- src/modules/voip/voip.module.ts
+++ src/modules/voip/voip.module.ts
@@ -3,12 +3,10 @@ import { AsteriskModule } from './modules/asterisk/asterisk.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Voip } from './entities/voip.entity';
 import { VoipService } from './services/voip.service';
-import { VoipController } from './controllers/voip-test.controller';
 
 @Module({
     imports: [TypeOrmModule.forFeature([Voip]), AsteriskModule],
     providers: [VoipService],
     exports: [VoipService],
-    controllers: [VoipController],
 })
 export class VoipModule {}
