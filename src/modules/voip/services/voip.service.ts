4d3d9e8fef1567c01927d98313fccc48560aae0e
ProVitSer <prokinvs@gmail.com>
2024-09-06 19:51:38 +0000

    ref

--- src/modules/voip/services/voip.service.ts
+++ src/modules/voip/services/voip.service.ts
@@ -1,21 +1,12 @@
-/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Inject, Injectable } from '@nestjs/common';
-import { VoipPbxService } from '../interfaces/voip.interface';
-import { TrunkType } from '../interfaces/voip.enum';
+import { CreateTrunkData, VoipPbxService } from '../interfaces/voip.interface';
 
 @Injectable()
 export class VoipService {
     constructor(@Inject('Asterisk') private readonly voipPbxService: VoipPbxService) {}
 
-    public async addNewTrunk(voip: any) {
-        const result = await this.voipPbxService.addTrunk({
-            clientId: 123121111,
-            trunkType: TrunkType.call,
-            authId: '111111111',
-            authPassword: 'mMa8Okme3z',
-            pbxIp: 'localhost',
-        });
-        console.log(result);
+    public async addNewTrunk(data: CreateTrunkData) {
+        await this.voipPbxService.addTrunk(data);
     }
 
     public async getTrunkStatus() {}
