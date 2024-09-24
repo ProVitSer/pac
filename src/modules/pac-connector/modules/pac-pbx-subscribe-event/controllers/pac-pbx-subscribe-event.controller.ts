c90e4f31fd3c3de65df62b0b6c11552294b8f4b7
ProVitSer <prokinvs@gmail.com>
2024-09-24 21:28:05 +0000

    feat(ppse): init start service\controller for precess incoming call info from pbx

--- src/modules/pac-connector/modules/pac-pbx-subscribe-event/controllers/pac-pbx-subscribe-event.controller.ts
+++ src/modules/pac-connector/modules/pac-pbx-subscribe-event/controllers/pac-pbx-subscribe-event.controller.ts
@@ -1,22 +1,25 @@
-import { Body, Controller, Post, Req } from '@nestjs/common';
+import { Body, Controller, Post, Req, UseGuards } from '@nestjs/common';
 import { PacPbxSubscribeEventNotificationService } from '../services/pac-pbx-subscribe-event-notification.service';
-import { Request } from 'express';
 import { PbxEvenetActiveConnectionsInfo } from '../interfaces/pac-pbx-subscribe-event.interface';
+import { PacCheckGuard } from '@app/modules/pac-connector/guards/pac-check.guard';
+import { RequestWithPacInfo } from '@app/modules/pac-connector/interfaces/pac-connector.interface';
 
+@UseGuards(PacCheckGuard)
 @Controller('pac-connector/pac-pbx-subscribe-event')
 export class PacPbxSubscribeEventController {
     constructor(private readonly ppsenService: PacPbxSubscribeEventNotificationService) {}
 
     @Post('delete')
-    async deleteEvent(@Req() request: Request, @Body() data: PbxEvenetActiveConnectionsInfo): Promise<void> {
-        await this.ppsenService.test(request, data);
+    async deleteEvent(@Req() request: RequestWithPacInfo, @Body() data: PbxEvenetActiveConnectionsInfo): Promise<void> {
+        this.ppsenService.deleteEventProcess(request, data);
     }
     @Post('update')
-    async updateEvent(@Req() request: Request, @Body() data: PbxEvenetActiveConnectionsInfo): Promise<void> {
-        await this.ppsenService.eventProcess(request, data);
+    async updateEvent(@Req() request: RequestWithPacInfo, @Body() data: PbxEvenetActiveConnectionsInfo): Promise<void> {
+        this.ppsenService.updateEventProcess(request, data);
     }
+
     @Post('insert')
-    async insertEvent(@Req() request: Request, @Body() data: PbxEvenetActiveConnectionsInfo): Promise<void> {
-        await this.ppsenService.eventProcess(request, data);
+    async insertEvent(@Req() request: RequestWithPacInfo, @Body() data: PbxEvenetActiveConnectionsInfo): Promise<void> {
+        this.ppsenService.insertEventProcess(request, data);
     }
 }
--- src/modules/pac-connector/modules/pac-pbx-subscribe-event/services/pac-pbx-subscribe-event-notification.service.ts
+++ src/modules/pac-connector/modules/pac-pbx-subscribe-event/services/pac-pbx-subscribe-event-notification.service.ts
@@ -1,49 +1,24 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { Injectable } from '@nestjs/common';
-import { PacSqlService } from '../../pac-sql/services/pac-sql.service';
 import { Request } from 'express';
-import { UtilsService } from '@app/common/utils/utils.service';
-import { ClientService } from '@app/modules/client/services/client.service';
-import { PacConnectorService } from '@app/modules/pac-connector/services/pac-connector.service';
 import { PacCallService } from '../../pac-call/services/pac-call.service';
 import { PbxEvenetActiveConnectionsInfo } from '../interfaces/pac-pbx-subscribe-event.interface';
 import { PbxEventActiveConnectionsInfoAdapter } from '../adapters/pbx-event-active-connections-info.adapter';
 import { ApiConnectionCallStatus } from '@app/modules/api/modules/call/interfaces/api-call.enum';
+import { RequestWithPacInfo } from '@app/modules/pac-connector/interfaces/pac-connector.interface';
 
 @Injectable()
 export class PacPbxSubscribeEventNotificationService {
-    constructor(
-        private readonly pacSqlService: PacSqlService,
-        private readonly pacConnectorService: PacConnectorService,
-        private readonly clientService: ClientService,
-        private readonly pacCallService: PacCallService,
-    ) {}
+    constructor(private readonly pacCallService: PacCallService) {}
 
-    public async eventProcess(request: Request, data: PbxEvenetActiveConnectionsInfo) {
-        const ip = UtilsService.convertToIPv4(request.ip);
-
-        const connector = await this.pacConnectorService.getPacConnectorByIp(ip);
-
-        if (!connector) return;
-
-        const client = await this.clientService.getClientById(connector.clientId);
-
-        if (!client) return;
-
+    public async insertEventProcess(request: RequestWithPacInfo, data: PbxEvenetActiveConnectionsInfo): Promise<void> {
         const activeConnectionsInfo = new PbxEventActiveConnectionsInfoAdapter(data).activeConnectionsInfo;
+    }
 
-        // TODO
-        // Получение маршрутного номера и номера группы/очереди из модуля smart routing
-
-        const call = activeConnectionsInfo[0].connectionsData.filter((d) => d.destinationNumber == '814');
-
-        if (call[0].connectionCallStatus == ApiConnectionCallStatus.CallConnected) {
-            const d = {
-                callId: activeConnectionsInfo[0].callId,
-                dn: '814',
-                numberTo: '107',
-            };
-
-            await this.pacCallService.transferCall(client, d);
-        }
+    public async updateEventProcess(request: RequestWithPacInfo, data: PbxEvenetActiveConnectionsInfo): Promise<void> {
+        const activeConnectionsInfo = new PbxEventActiveConnectionsInfoAdapter(data).activeConnectionsInfo;
+    }
+    public async deleteEventProcess(request: RequestWithPacInfo, data: PbxEvenetActiveConnectionsInfo): Promise<void> {
+        const activeConnectionsInfo = new PbxEventActiveConnectionsInfoAdapter(data).activeConnectionsInfo;
     }
 }
