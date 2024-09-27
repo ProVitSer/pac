e6be05d7abb5bed26de937de2c2b6b07513d1b31
ProVitSer <prokinvs@gmail.com>
2024-09-27 17:16:59 +0000

    update migration

--- src/database/migrations/1726854834445-pac.ts
+++ src/database/migrations/1726854834445-pac.ts
@@ -1,7 +1,7 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Pac1726854834445 implements MigrationInterface {
-    name = 'Pac1726854834445'
+    name = 'Pac1726854834445';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "tg_config" DROP COLUMN "message"`);
@@ -10,5 +10,4 @@ export class Pac1726854834445 implements MigrationInterface {
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "tg_config" ADD "message" character varying`);
     }
-
 }
--- src/database/migrations/1726858196089-pac.ts
+++ src/database/migrations/1726858196089-pac.ts
@@ -1,16 +1,19 @@
-import { MigrationInterface, QueryRunner } from "typeorm";
+import { MigrationInterface, QueryRunner } from 'typeorm';
 
 export class Pac1726858196089 implements MigrationInterface {
-    name = 'Pac1726858196089'
+    name = 'Pac1726858196089';
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "tg_config_tg_users_tg_users" DROP CONSTRAINT "FK_8cfd4cdc035baac89b6f1881e19"`);
-        await queryRunner.query(`ALTER TABLE "tg_config_tg_users_tg_users" ADD CONSTRAINT "FK_8cfd4cdc035baac89b6f1881e19" FOREIGN KEY ("tgUsersId") REFERENCES "tg_users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`);
+        await queryRunner.query(
+            `ALTER TABLE "tg_config_tg_users_tg_users" ADD CONSTRAINT "FK_8cfd4cdc035baac89b6f1881e19" FOREIGN KEY ("tgUsersId") REFERENCES "tg_users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        );
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(`ALTER TABLE "tg_config_tg_users_tg_users" DROP CONSTRAINT "FK_8cfd4cdc035baac89b6f1881e19"`);
-        await queryRunner.query(`ALTER TABLE "tg_config_tg_users_tg_users" ADD CONSTRAINT "FK_8cfd4cdc035baac89b6f1881e19" FOREIGN KEY ("tgUsersId") REFERENCES "tg_users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`);
+        await queryRunner.query(
+            `ALTER TABLE "tg_config_tg_users_tg_users" ADD CONSTRAINT "FK_8cfd4cdc035baac89b6f1881e19" FOREIGN KEY ("tgUsersId") REFERENCES "tg_users"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
+        );
     }
-
 }
