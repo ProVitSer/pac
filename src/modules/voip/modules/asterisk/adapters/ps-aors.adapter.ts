cae019415b195b87c00ce0c6ed32b57513c2bc6e
ProVitSer <prokinvs@gmail.com>
2024-10-01 19:40:24 +0000

    feat(asterisk): add contact for qualify trunk

--- src/modules/voip/modules/asterisk/adapters/ps-aors.adapter.ts
+++ src/modules/voip/modules/asterisk/adapters/ps-aors.adapter.ts
@@ -5,10 +5,11 @@ export class PsAorsAdapter {
     id: string;
     maxContacts: number;
     qualifyFrequency: number;
-
+    contact: string;
     constructor(data: CreateTrunkDataWithTrunkId) {
         this.id = data.trunkId;
         this.maxContacts = DEFAULT_MAX_CONTACTS;
         this.qualifyFrequency = DEFAULT_QUALIFY_FREQUENCY;
+        this.contact = `sip:${data.pbxIp}`;
     }
 }
