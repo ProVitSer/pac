b2266a2c669fb42924104f0c76ba94c834b6f68d
ProVitSer <prokinvs@gmail.com>
2024-10-19 19:30:29 +0000

    ref

--- src/modules/voip/controllers/voip.controller.ts
+++ src/modules/voip/controllers/voip.controller.ts
@@ -16,27 +16,27 @@ export class VoipController {
     constructor(private readonly voipService: VoipService) {}
 
     @Post('trunk')
-    async createTrunk(@Req() request: RequestWithUser, @Body() data: CreateTrunkDto): Promise<TrunkStatusResult> {
-        return this.voipService.addNewTrunk({ ...data, client: request.user.client });
+    async createTrunk(@Req() req: RequestWithUser, @Body() data: CreateTrunkDto): Promise<TrunkStatusResult> {
+        return this.voipService.addNewTrunk({ ...data, client: req.user.client });
     }
 
     @Get('trunk/:trunkId')
-    async getTrunkStatusById(@Req() request: RequestWithUser, @Param('trunkId') trunkId: string): Promise<TrunkStatusResult> {
+    async getTrunkStatusById(@Req() req: RequestWithUser, @Param('trunkId') trunkId: string): Promise<TrunkStatusResult> {
         return this.voipService.getTrunkStatusById(trunkId);
     }
 
     @Get('trunk')
-    async getTrunks(@Req() request: RequestWithUser): Promise<Voip[]> {
-        return this.voipService.getTrunks(request.user.client);
+    async getTrunks(@Req() req: RequestWithUser): Promise<Voip[]> {
+        return this.voipService.getTrunks(req.user.client);
     }
 
     @Delete('trunk/:trunkId')
-    async deleteTrunk(@Req() request: RequestWithUser, @Param('trunkId') trunkId: string) {
-        return this.voipService.deleteTrunk(request.user.client, trunkId);
+    async deleteTrunk(@Req() req: RequestWithUser, @Param('trunkId') trunkId: string) {
+        return this.voipService.deleteTrunk(req.user.client, trunkId);
     }
 
     @Put('trunk')
-    async updateTrunk(@Req() request: RequestWithUser, @Body() trunkData: UpdateTrunkDto): Promise<TrunkStatusResult> {
-        return this.voipService.updateTrunk(request.user.client, trunkData);
+    async updateTrunk(@Req() req: RequestWithUser, @Body() trunkData: UpdateTrunkDto): Promise<TrunkStatusResult> {
+        return this.voipService.updateTrunk(req.user.client, trunkData);
     }
 }
